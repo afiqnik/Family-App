2cc33f3531a409be32a5fe3677e69256e11ea741
Nik Afiq <afiqharipin@gmail.com>
2024-01-19 15:56:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+class FamilyMember {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  } 
+}
+
+const familyMembers = [];
+
+function submitFamilyMember() {
+  const nameInput = document.getElementById("name");
+  const ageInput = document.getElementById("age");
+  const genderInput = document.getElementById("gender");
+  const name = nameInput.value;
+  const age = ageInput.value;
+  const gender = genderInput.value;
+
+  const familyMember = new FamilyMember(name, age, gender);
+  familyMembers.push(familyMember);
+  reloadFamilyMembers();
+}
+
+function reloadFamilyMembers() {
+  const familyList = document.getElementById("familyMembers").tBodies[1];
+  familyList.innerHTML = "";
+
+  for (const familyMember of familyMembers) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${familyMember.name}</td>
+      <td>${familyMember.age}</td>
+      <td>${familyMember.gender}</td>
+    `;
+    familyList.appendChild(row);
+  }
+}
